fac15eb1d2ab06d67630298155f750f5da8f5f3c
bichotll <jaume@bic.cat>
2012-09-01 21:09:24 +0200

    base vista llibre

--- lib/config.php
+++ lib/config.php
@@ -9,7 +9,7 @@ add_theme_support('bootstrap-top-navbar');  // Enable Bootstrap's fixed navbar
 
 // Define which pages shouldn't have the sidebar
 function roots_sidebar() {
-  if (is_404() || is_page_template('page-custom.php')) {
+  if (is_404() || is_page_template('page-custom.php') || is_singular('llibre') ) {
     return false;
   } else {
     return true;
